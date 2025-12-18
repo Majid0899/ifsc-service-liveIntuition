f98fe8d07afd4eeb3f18e86cc6e5aad5d706c15f
Majid0899 <majidkhan0899@gmail.com>
2025-12-18 15:09:50 +0530

    feature:  create a model for users

---
+++ model/User.js
@@ -0,0 +1,25 @@
+import mongoose  from "mongoose";
+
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        tirm:true,
+        lowercase:true
+    },
+    password:{
+        type:String,
+        required:true,
+    }
+})
+
+const User=mongoose.Model("User",userSchema)
+
+export default User;
